4d953ce29547e6ffb0bda927a29c74e84028a9c8
Vladislav <xouli@list.ru>
2021-05-18 20:25:30 +0700

    looking

--- #src/js/burger.js
+++ #src/js/burger.js
@@ -1,1 +1,23 @@
-// alert('hello')
+let span =  document.querySelectorAll('.burger__span')[1]; 
+let burger = document.querySelector('.burger__view');
+burger.addEventListener('click',()=>{
+   
+   let burger__body = document.querySelector('.burger__body');
+   let nav = document.querySelector('.header__list');
+  
+   let privu = document.querySelector('.burger__body');
+   if(span.style.display == 'none'){
+      span.style.display = "block";
+   privu.style.display = 'none'
+   
+   }else{
+         span.style.display = 'none';
+         privu.style.display = "block";
+          burger__body.innerHTML = nav.innerHTML;
+   }
+   // burger__body.innerHTML = nav.innerHTML;
+   document.querySelector('.burger__span.__top').classList.toggle('_active');
+   document.querySelector('.burger__span.__bottom').classList.toggle('_active');
+  
+  
+})
--- dist/js/script.js
+++ dist/js/script.js
@@ -1,21 +1,3 @@
-// alert('hello');
-function testWebP(callback) {
-
-   var webP = new Image();
-   webP.onload = webP.onerror = function () {
-   callback(webP.height == 2);
-   };
-   webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
-   }
-   
-   testWebP(function (support) {
-   
-   if (support == true) {
-   document.querySelector('body').classList.add('webp');
-   }else{
-   document.querySelector('body').classList.add('no-webp');
-   }
-   });;
 let span =  document.querySelectorAll('.burger__span')[1]; 
 let burger = document.querySelector('.burger__view');
 burger.addEventListener('click',()=>{
@@ -39,5 +21,23 @@ burger.addEventListener('click',()=>{
   
   
 })
+;
+function testWebP(callback) {
+
+   var webP = new Image();
+   webP.onload = webP.onerror = function () {
+   callback(webP.height == 2);
+   };
+   webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+   }
+   
+   testWebP(function (support) {
+   
+   if (support == true) {
+   document.querySelector('body').classList.add('webp');
+   }else{
+   document.querySelector('body').classList.add('no-webp');
+   }
+   });;
 
 
